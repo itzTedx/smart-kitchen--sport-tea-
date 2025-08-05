76867e3c31d66bea046c7d95eb0767afb8ac3225
melwin <114172287+itzTedx@users.noreply.github.com>
2025-08-05 14:20:48 +0400

    Add CTA and Footer components to RootLayout; remove footer from Home page

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-secondary p-3 text-center text-secondary-foreground">
+      <h5>2025 © Copyright, Sport Tea LLC.</h5>
+    </footer>
+  );
+};
